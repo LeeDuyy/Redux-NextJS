3acb6e2630e966f8735ffa7affab3db47f920d51
DESKTOP-1PQIV2V\Admin <leduy221200@gmail.com>
2022-04-28 15:14:30 +0700

    cm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { NextComponentType, NextPageContext } from 'next'
+import { ComponentType, Fragment } from 'react'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import Layout from '../src/Layout'
+config.autoAddCss = false
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type ToDoAppProps = AppProps & {
+    Component: NextComponentType<NextPageContext, any> & {
+        Layout: ComponentType
+    }
+}
+
+function MyApp({ Component }: ToDoAppProps) {
+    return (
+        <Fragment>
+            <Layout/>
+            <Component />
+        </Fragment>
+    )
 }
 
 export default MyApp
